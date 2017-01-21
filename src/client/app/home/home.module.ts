d94939dfe9a3da701e72e91f8fd27fe07e733a58
amalshehu <amalshehu@gmail.com>
2017-01-21 13:43:26 +0530

    File structure change

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from '../shared/shared.module';
 import { NameListService } from '../shared/name-list/name-list.service';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, SharedModule],
+  imports: [CommonModule, HomeRoutingModule, SharedModule, MaterialModule.forRoot()],
   declarations: [HomeComponent],
   exports: [HomeComponent],
   providers: [NameListService]
--- src/client/app/login/login-routing.module.ts
+++ src/client/app/login/login-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login.component';
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: '', component: LoginComponent }
+      { path: 'login', component: LoginComponent }
     ])
   ],
   exports: [RouterModule]
