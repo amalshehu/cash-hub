d4edc234f735087d39dc11f1eebe518c7f263113
amalshehu <amalshehu@gmail.com>
2017-02-04 19:50:23 +0530

    Update setting

--- src/client/app/login/login.service.ts
+++ src/client/app/login/login.service.ts
@@ -1,42 +1,42 @@
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
- 
+
 export class User {
   constructor(
-    public email: string,
-    public password: string) { }
+    public email: any,
+    public password: any) { }
 }
- 
+
 var users = [
   new User('admin@admin.com','admin'),
   new User('user1@gmail.com','a23')
 ];
- 
+
 @Injectable()
 export class AuthenticationService {
- 
+
   constructor(
     private _router: Router){}
- 
+
   logout() {
-    localStorage.removeItem("user");
+    localStorage.removeItem('user');
     this._router.navigate(['Login']);
   }
- 
-  login(user){
+
+  login(user:any){
     var authenticatedUser = users.find(u => u.email === user.email);
     if (authenticatedUser && authenticatedUser.password === user.password){
       localStorage.setItem("user", authenticatedUser);
-      this._router.navigate(['home']);      
+      this._router.navigate(['Home']);
       return true;
     }
     return false;
- 
+
   }
- 
+
    checkCredentials(){
     if (localStorage.getItem("user") === null){
         this._router.navigate(['Login']);
     }
-  } 
+  }
 }
